d3a5ff2d9a3f7c906529728fb71ec4a8f9767cd8
hmmccabe <heather.m.mccabe@intel.com>
2018-04-10 12:57:42 -0700

    swap globalgetopt names
    Signed-off-by: hmmccabe <heather.m.mccabe@intel.com>

--- apps/doc_examples/C/NCAPI_2/cpp/globalgetopt2.cpp
+++ apps/doc_examples/C/NCAPI_2/cpp/globalgetopt2.cpp
@@ -4,19 +4,37 @@
 
 int main(int argc, char** argv) {
     ncStatus_t retCode;
-    int logLevel;
-    unsigned int dataLength = sizeof(logLevel);
+    unsigned int* apiVersion = NULL;
+    unsigned int dataLength = 0;
 
-    // Get the logging level option value
-    retCode = ncGlobalGetOption(NC_RW_LOG_LEVEL, &logLevel, &dataLength);
+    // Call once to get the correct dataLength
+    retCode = ncGlobalGetOption(NC_RO_API_VERSION, apiVersion, &dataLength);
 
-    // retCode should be NC_OK unless there was a problem
+    // retCode should be NC_INVALID_DATA_LENGTH unless there was another problem
+    if(retCode != NC_INVALID_DATA_LENGTH)
+    {
+        printf("Error: %d\n", retCode);
+        exit(-1);
+    }
+
+    // Now the value of dataLength is correctly set (in this case for an array of 4 unsigned ints)
+    // Allocate the array buffer
+    apiVersion = (unsigned int*)malloc(dataLength);
+
+    // Call the function again to get the API version option value
+    retCode = ncGlobalGetOption(NC_RO_API_VERSION, apiVersion, &dataLength);
+    
+    // Now retCode should be NC_OK unless there was another problem
     if(retCode != NC_OK)
     {
         printf("Error: %d\n", retCode);
         exit(-1);
     }
 
-    // Use the log level as needed
-    printf("LOGGING LEVEL IS %d\n", logLevel);
+    // Use the API version as needed
+    printf("API VERSION - Major: %d, Minor: %d, Hotfix: %d, Release: %d\n", apiVersion[0], apiVersion[1], apiVersion[2], apiVersion[3]);
+
+    // When you are done, free it and set the pointer to NULL
+    free(apiVersion);
+    apiVersion = NULL;
 }
