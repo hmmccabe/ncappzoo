591b707959575c18437bde994cdf72152c60cbbd
hmmccabe <heather.m.mccabe@intel.com>
2018-04-10 14:48:56 -0700

    split build and run script
    Signed-off-by: hmmccabe <heather.m.mccabe@intel.com>

--- apps/doc_examples/C/NCAPI_2/build.sh
+++ apps/doc_examples/C/NCAPI_2/build.sh
@@ -4,5 +4,11 @@ mkdir -p build
 #g++ cpp/simple_cpp_withconv_ncapi2.cpp -o build/simple_cpp_with_conv -lmvnc
 
 gcc -Wall -Wextra -std=c89 -pedantic -Wmissing-prototypes -Wstrict-prototypes -Wold-style-definition ./src/devicegetopt1.c -o ./build/devicegetopt1 -lmvnc
+gcc -Wall -Wextra -std=c89 -pedantic -Wmissing-prototypes -Wstrict-prototypes -Wold-style-definition ./src/devicegetopt2.c -o ./build/devicegetopt2 -lmvnc
+gcc -Wall -Wextra -std=c89 -pedantic -Wmissing-prototypes -Wstrict-prototypes -Wold-style-definition ./src/fifogetopt1.c -o ./build/fifogetopt1 -lmvnc
+gcc -Wall -Wextra -std=c89 -pedantic -Wmissing-prototypes -Wstrict-prototypes -Wold-style-definition ./src/fifogetopt2.c -o ./build/fifogetopt2 -lmvnc
+gcc -Wall -Wextra -std=c89 -pedantic -Wmissing-prototypes -Wstrict-prototypes -Wold-style-definition ./src/graphgetopt1.c -o ./build/graphgetopt1 -lmvnc
+gcc -Wall -Wextra -std=c89 -pedantic -Wmissing-prototypes -Wstrict-prototypes -Wold-style-definition ./src/graphgetopt2.c -o ./build/graphgetopt2 -lmvnc
 gcc -Wall -Wextra -std=c89 -pedantic -Wmissing-prototypes -Wstrict-prototypes -Wold-style-definition ./src/globalgetopt1.c -o ./build/globalgetopt1 -lmvnc
 gcc -Wall -Wextra -std=c89 -pedantic -Wmissing-prototypes -Wstrict-prototypes -Wold-style-definition ./src/globalgetopt2.c -o ./build/globalgetopt2 -lmvnc
+
---
+++ apps/doc_examples/C/NCAPI_2/run.sh
@@ -0,0 +1,8 @@
+./build/devicegetopt1
+#./build/devicegetopt2
+./build/fifogetopt1
+#./build/fifogetopt2
+./build/graphgetopt1
+#./build/graphgetopt2
+./build/globalgetopt1
+#./build/globalgetopt2
