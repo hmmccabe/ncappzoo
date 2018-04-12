d5d0262d6957a16c3af392944f8daf4d568c162e
hmmccabe <heather.m.mccabe@intel.com>
2018-04-12 15:03:01 -0700

    update comments
    Signed-off-by: hmmccabe <heather.m.mccabe@intel.com>

--- apps/doc_examples/Python/NCAPI_2/device_ex.py
+++ apps/doc_examples/Python/NCAPI_2/device_ex.py
@@ -6,10 +6,10 @@ device_handles = mvncapi.enumerate_devices()
 # Create a Device object for the first device found
 device = mvncapi.Device(device_handles[0])
 
-# Initialize the device
+# Open communication with the device
 device.open()
 
-# Initialize a graph container
+# Create a Graph
 graph = Graph('graph1')
 
 # Read a compiled network graph from file (set the graph_filepath correctly for your graph file)
@@ -17,7 +17,7 @@ graph_filepath = './graph'
 with open(graph_filepath, 'rb') as f:
     graph_buffer = f.read()
 
-# Allocate the network graph on the device
+# Allocate the graph on the device
 device.graph_allocate(graph, graph_buffer)
 
 #
