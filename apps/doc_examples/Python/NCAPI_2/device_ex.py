1a91b4c3c0d4ec6abc2164574d879237da37f0cf
hmmccabe <heather.m.mccabe@intel.com>
2018-04-12 15:08:00 -0700

    add get_option
    Signed-off-by: hmmccabe <heather.m.mccabe@intel.com>

--- apps/doc_examples/Python/NCAPI_2/device_ex.py
+++ apps/doc_examples/Python/NCAPI_2/device_ex.py
@@ -6,6 +6,9 @@ device_handles = mvncapi.enumerate_devices()
 # Create a Device object for the first device found
 device = mvncapi.Device(device_handles[0])
 
+# Get the device name and output to console
+print(device.get_option(mvncapi.DeviceOption.RO_DEVICE_NAME))
+
 # Open communication with the device
 device.open()
 
